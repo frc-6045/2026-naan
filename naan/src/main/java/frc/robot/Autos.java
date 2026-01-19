dda905e3c09ed981b6acf2283387db8e8bf9c398
Seeinq <ayusfun1@gmail.com>
2026-01-19 10:09:05 -0600

    Update Autos.java

--- naan/src/main/java/frc/robot/Autos.java
+++ naan/src/main/java/frc/robot/Autos.java
@@ -4,17 +4,28 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.NamedCommands;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
-public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return new InstantCommand();
+public class Autos {
+
+  /**
+   * TO REGISTER A COMMAND IN PATHPLANNER
+   * NamedCommands.registerCommand("autoCommandName", new exampleCommand(parameters));
+   * exampleCommand must be closed-loop
+   * PID stuff (untimed commmands) should use .asProxy();
+   * 
+   * ADD AUTO TO AUTO CHOOSER
+   * autoChooser.addOption("exampleAutoName", AutoBuilder.buildAuto("NameOfAutoInPathplanner"));
+   */
+  public Autos() {
+
   }
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
+  public Command getAutonomousCommand() {
+    return autoChooser.getSelected();
   }
 }
