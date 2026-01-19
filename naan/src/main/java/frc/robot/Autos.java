4ee9b249f8f1096888cf2dde4557f02781e6205d
Seeinq <ayusfun1@gmail.com>
2026-01-19 10:18:19 -0600

    Updated Autos.java further, more stuff with subsystems/commands

--- naan/src/main/java/frc/robot/Autos.java
+++ naan/src/main/java/frc/robot/Autos.java
@@ -4,13 +4,16 @@
 
 package frc.robot;
 
-import com.pathplanner.lib.auto.NamedCommands;
-
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Spindexer;
 
 public class Autos {
+  private final Intake m_Intake;
+    private final Spindexer m_Spindexer;
+
+    private SendableChooser<Command> autoChooser;
 
   /**
    * TO REGISTER A COMMAND IN PATHPLANNER
@@ -21,11 +24,13 @@ public class Autos {
    * ADD AUTO TO AUTO CHOOSER
    * autoChooser.addOption("exampleAutoName", AutoBuilder.buildAuto("NameOfAutoInPathplanner"));
    */
-  public Autos() {
-
+  public Autos(Intake intake, Spindexer spindexer) {
+    m_Intake=intake;
+    m_Spindexer=spindexer;
   }
 
   public Command getAutonomousCommand() {
+    autoChooser = new SendableChooser<Command>();
     return autoChooser.getSelected();
   }
 }
--- naan/src/main/java/frc/robot/subsystems/Spindexer.java
+++ naan/src/main/java/frc/robot/subsystems/Spindexer.java
@@ -1,5 +1,55 @@
 package frc.robot.subsystems;
 
-public class Spindexer {
-    
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.MotorConstants;
+
+public class Spindexer extends SubsystemBase {
+  private final SparkFlex m_SpindexerMotor;
+  SparkFlexConfig config = new SparkFlexConfig();
+
+  public Spindexer() {
+    m_SpindexerMotor = new SparkFlex(67, MotorType.kBrushless);
+
+    updateMotorSettings(m_SpindexerMotor);
+    m_SpindexerMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+   public void updateMotorSettings(SparkFlex motor) {
+    config
+        .idleMode(IdleMode.kBrake)
+        //.smartCurrentLimit(MotorConstants.kSpindexerCurrentLimit)
+        ;
+    config.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder);
+  }
+
+  public void setSpeed(double speed) {
+    //speed = MathUtil.clamp(-MotorConstants.kSpindexerMotorMaximumSpeed,MotorConstants.kSpindexerMotorMaximumSpeed);
+    m_SpindexerMotor.set(speed);
+    SmartDashboard.putNumber("Climb speed", speed);
+  }
+
+  public void stopClimbMotor() {
+    m_SpindexerMotor.stopMotor();
+    SmartDashboard.putNumber("Climb speed", 0);
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+
+  @Override
+  public void simulationPeriodic() {}
 }
